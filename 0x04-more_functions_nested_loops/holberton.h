640c482b1c2366f6a0de68100864565eb86ab4b1
Super User <kidus.efr@gmail.com>
2021-09-02 09:25:00 -0700

    commit

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,11 +1,11 @@
-#ifndef _HOLBERTON_H_
-#define _HOLBERTON_H_
+#ifndef FILE_HOLBERTON
+#define FILE_HOLBERTON
 
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
-int _putchar(char);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
