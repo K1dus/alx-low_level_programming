24b9b2356a2f98cedf95a89351a06efc38d7fe71
Super User <kidus.efr@gmail.com>
2021-09-30 12:47:38 -0700

    commit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,26 +1,22 @@
-#include "variadic_functions.h"
-#include <stdio.h>
+#ifndef _VARIADIC_FUNCTIONS_H_
+#define _VARIADIC_FUNCTIONS_H_
+
 #include <stdarg.h>
 /**
- * sum_them_all - sum all arguements
- * @n: number of arguements
- * Return: 0 is n is 0 or return sum
+ * struct print - print type with corresponding print function
+ * @t: print type
+ * @f: print function
  */
-
-int sum_them_all(const unsigned int n, ...)
+typedef struct print
 {
-	va_list numbers;
-	int sum;
-	unsigned int i;
+	char *t;
+	void (*f)(va_list);
+} print_t;
 
-	if (n == 0)
-		return (0);
-	sum = 0;
-	va_start(numbers, n);
-	for (i = 0; i < n; i++)
-	{
-		sum += va_arg(numbers, int);
-	}
-	va_end(numbers);
-	return (sum);
-}
+int _putchar(char);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif /* _VARIADIC_FUNCTIONS_H_ */
